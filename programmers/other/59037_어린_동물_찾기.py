94905a3c317ff322103f2431dd1d623d38a6299a
dlghdud1433 <dlghdud1433@gmail.com>
2026-01-19 23:02:04 +0900

    [프로그래머스] 어린 동물 찾기 풀이 제출
    - 문제 ID: 59037
    - 난이도: unknown
    - 제출일: 2026. 01. 19. 23:02:03
    - 작성자: 이호영

--- programmers/other/59037_어린_동물_찾기.py
+++ programmers/other/59037_어린_동물_찾기.py
@@ -2,7 +2,7 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59037
 # 작성자: 이호영
-# 작성일: 2026. 01. 19. 23:01:55
+# 작성일: 2026. 01. 19. 23:02:03
 
 -- 코드를 입력하세요
 SELECT ANIMAL_ID,
