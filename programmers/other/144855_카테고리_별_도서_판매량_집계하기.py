c77fa8bf52cdefabf3631c7305e5257585c35f44
dlghdud1433 <dlghdud1433@gmail.com>
2026-01-19 23:12:26 +0900

    [프로그래머스] 카테고리 별 도서 판매량 집계하기 풀이 제출
    - 문제 ID: 144855
    - 난이도: unknown
    - 제출일: 2026. 01. 19. 23:12:25
    - 작성자: 이호영

--- programmers/other/144855_카테고리_별_도서_판매량_집계하기.py
+++ programmers/other/144855_카테고리_별_도서_판매량_집계하기.py
@@ -2,7 +2,7 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/144855
 # 작성자: 이호영
-# 작성일: 2026. 01. 19. 23:12:16
+# 작성일: 2026. 01. 19. 23:12:25
 
 -- 코드를 입력하세요
 SELECT b.CATEGORY,
