aeda9bad421c1072a135b6e8b935e88c2c808269
dlghdud1433 <dlghdud1433@gmail.com>
2026-01-19 23:08:36 +0900

    [프로그래머스] 조건에 맞는 도서 리스트 출력하기 풀이 제출
    - 문제 ID: 144853
    - 난이도: unknown
    - 제출일: 2026. 01. 19. 23:08:35
    - 작성자: 이호영

--- programmers/other/144853_조건에_맞는_도서_리스트_출력하기.py
+++ programmers/other/144853_조건에_맞는_도서_리스트_출력하기.py
@@ -2,7 +2,7 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/144853
 # 작성자: 이호영
-# 작성일: 2026. 01. 19. 23:08:24
+# 작성일: 2026. 01. 19. 23:08:35
 
 -- 코드를 입력하세요
 SELECT BOOK_ID,
