441290ae7f8d5a9cd330160ff6df47d60d8d7eaa
dlghdud1433 <dlghdud1433@gmail.com>
2026-01-19 23:01:50 +0900

    [프로그래머스] 최솟값 구하기 풀이 제출
    - 문제 ID: 59038
    - 난이도: unknown
    - 제출일: 2026. 01. 19. 23:01:49
    - 작성자: 이호영

--- programmers/other/59038_최솟값_구하기.py
+++ programmers/other/59038_최솟값_구하기.py
@@ -2,7 +2,7 @@
 # 프로그래머스 (unknown)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59038
 # 작성자: 이호영
-# 작성일: 2026. 01. 19. 23:01:43
+# 작성일: 2026. 01. 19. 23:01:49
 
 -- 코드를 입력하세요
 SELECT DATETIME
